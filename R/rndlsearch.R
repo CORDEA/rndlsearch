86ed8454c6c88824beb1c5ca37620c7a13968654
Yoshihiro Tanaka <contact@cordea.jp>
2022-08-06 21:46:38 +1000

    parse xml

--- R/rndlsearch.R
+++ R/rndlsearch.R
@@ -1,7 +1,9 @@
 #' @export
 rndlsearch <- function(...) {
-  httr::GET(
+  response <- httr::GET(
     url = "https://iss.ndl.go.jp/api/opensearch",
     query = list(...)
   )
+  xml <- xml2::read_xml(response)
+  xml2::xml_find_all(xml, "//item")
 }
