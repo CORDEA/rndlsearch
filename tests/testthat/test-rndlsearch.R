a05cfed92104669582baaffcee2536b024d190f8
Yoshihiro Tanaka <contact@cordea.jp>
2022-08-07 16:14:35 +1000

    add tests

--- tests/testthat/test-rndlsearch.R
+++ tests/testthat/test-rndlsearch.R
@@ -1,3 +1,41 @@
-test_that("", {
+test_that("parse response", {
+  xml <- xml2::read_xml("test.xml")
+  parsed <- parse_response(xml)
 
+  expect_equal(length(parsed), 3)
+
+  expect_equal(sum(lengths(parsed[[1]])), 0)
+
+  second = parsed[[2]]
+  expect_equal(sum(lengths(second)), 15)
+  expect_equal(second$Title, "Title")
+  expect_equal(second$Link, "Link")
+  expect_equal(second$Descriptions, list("Description"))
+  expect_equal(second$Author, "Author")
+  expect_equal(second$`Publish Date`, "PubDate")
+  expect_equal(second$Category, "Category")
+  expect_equal(second$GUID, "GUID")
+  expect_equal(second$Date, "Date")
+  expect_equal(second$Creators, list("Creator"))
+  expect_equal(second$Subjects, list("Subject"))
+  expect_equal(second$Volume, "Volume")
+  expect_equal(second$Edition, "Edition")
+  expect_equal(second$`Series Title`, "SeriesTitle")
+  expect_equal(second$Publishers, list("Publisher"))
+  expect_equal(second$`Issued Date`, "Issued")
+
+  third = parsed[[3]]
+  expect_equal(sum(lengths(third)), 19)
+  expect_equal(third$Title, "Title")
+  expect_equal(third$Link, "Link")
+  expect_equal(third$Author, "Author")
+  expect_equal(third$Category, "Category")
+  expect_equal(third$GUID, "GUID")
+  expect_equal(third$Descriptions, list("Description1", "Description2"))
+  expect_equal(third$Creators, list("Creator1", "Creator2"))
+  expect_equal(third$Subjects, list("Subject1", "Subject2"))
+  expect_equal(third$Publishers, list("Publisher1", "Publisher2"))
+  expect_equal(third$Extents, list("Extent1", "Extent2"))
+  expect_equal(third$ISBN, "ISBN")
+  expect_equal(third$Resources, list("Reference1", "Reference2", "Reference3"))
 })
