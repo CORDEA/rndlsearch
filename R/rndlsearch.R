d1de9731194d5d86e9606da9dd574e6ce69e20aa
Yoshihiro Tanaka <contact@cordea.jp>
2022-08-07 14:52:40 +1000

    parse response

--- R/rndlsearch.R
+++ R/rndlsearch.R
@@ -5,5 +5,30 @@ rndlsearch <- function(...) {
     query = list(...)
   )
   xml <- xml2::read_xml(response)
-  xml2::xml_find_all(xml, "//item")
+  parse_response(xml)
+}
+
+parse_response <- function(response) {
+  items <- xml2::xml_find_all(response, "//item")
+  lapply(xml2::as_list(items), flatten_item)
+}
+
+flatten_item <- function(item) {
+  list(
+    item$title[[1]],
+    item$link[[1]],
+    item$description[[1]],
+    item$author[[1]],
+    item$category[[1]],
+    item$guid[[1]],
+    item$pubDate[[1]],
+    unname(unlist(item[names(item)=="creator"], recursive=FALSE)),
+    item$volume[[1]],
+    item$edition[[1]],
+    item$publisher[[1]],
+    item$date[[1]],
+    unname(lapply(item[names(item)=="seeAlso"], function (e) {
+      attributes(e)$resource
+    }))
+  )
 }
